4cb71ed6f09b65f6c3ae5078b33959806e0c6772
Shade Software <121112889+shadeappworld@users.noreply.github.com>
2024-05-03 17:55:40 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,6 +1,6 @@
 self.addEventListener('install', (event) => {
 	event.waitUntil(async function() {
-		const cache = await caches.open('notecards-4');
+		const cache = await caches.open('notecards-5');
 		await cache.addAll([
 			'./index.html',
 			'./global.css',
